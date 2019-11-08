6fac5ce50d07866c37b8103a6630b95da49596b8
Magnus Conrad Hyll <magnus@hyll.no>
2019-11-08 17:04:05 +0100

    Put serverctl functions inside class

--- minecraft/serverctl.py
+++ minecraft/serverctl.py
@@ -3,29 +3,8 @@
 from mcrcon import mcrcon
 import argparse
 import socket
-import errno
 import subprocess
 import time
-
-
-class RCON:
-    def __init__(self, password: str, host='localhost', port=25575):
-        self.password = password
-        self.host = host
-        self.port = port
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    def __enter__(self):
-        self.sock.connect((self.host, self.port))
-        if not mcrcon.login(self.sock, self.password):
-            raise Exception('Wrong RCON password')
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.sock.__exit__()
-
-    def cmd(self, command: str) -> str:
-        return mcrcon.command(self.sock, command)
 
 
 def seconds_to_str(s: int) -> str:
@@ -58,29 +37,57 @@ def seconds_to_str(s: int) -> str:
         return ', '.join(result[:-1]) + ' and ' + result[-1]
 
 
-def start_server(kill_session=False):
-    """
-    Starts the minecraft server in a tmux session.
-    """
-    if kill_session:
-        subprocess.run(['tmux', 'kill-session', '-t', 'mc'])
+class RCON:
+    def __init__(self, host: str, port: int, password: str):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((host, port))
+        if not mcrcon.login(self.sock, password):
+            raise Exception('Wrong RCON password')
 
-    subprocess.run(['tmux', 'new-session', '-d', '-s', 'mc', 'bash --init-file ServerStart.sh'], cwd='/home/magnus/ftb_server')
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.sock.__exit__()
+
+    def cmd(self, command: str) -> str:
+        return mcrcon.command(self.sock, command)
 
 
-def main(args):
-    rcon = RCON(args.password, args.host, args.port)
+class ServerCtl:
+    def __init__(self, rcon_host: str=None, rcon_port: int=None, rcon_password: str=None):
+        self.rcon_host = rcon_host
+        self.rcon_port = rcon_port
+        self.rcon_password = rcon_password
 
-    if args.start:
-        start_server()
+    def _create_rcon_connection(self) -> RCON:
+        return RCON(self.rcon_host, self.rcon_port, self.rcon_password)
 
-    elif args.notify_restart:
-        with rcon:
+    def start_server(self, kill_session=False):
+        """
+        Starts the minecraft server in a tmux session.
+        """
+
+        if kill_session:
+            subprocess.run(['tmux', 'kill-session', '-t', 'mc'])
+
+        subprocess.run(['tmux', 'new-session', '-d', '-s', 'mc', 'bash --init-file ServerStart.sh'], cwd='/home/magnus/ftb_server')
+
+    def notify_restart(self, seconds: int):
+        """
+        Sends notification to users on the server that it will restart in `seconds`.
+        """
+
+        with self._create_rcon_connection() as rcon:
             rcon.cmd('say Server will restart in {}'.format(seconds_to_str(args.notify_restart)))
 
-    elif args.restart:
+    def restart(self):
+        """
+        Restarts the server.
+        """
+
         try:
-            with rcon:
+            with self._create_rcon_connection() as rcon:
                 rcon.cmd('say Server will restart now')
                 rcon.cmd('save-all')
                 rcon.cmd('stop')
@@ -90,11 +97,15 @@ def main(args):
 
         # Wait some seconds for the server to shut down before restarting it
         time.sleep(5)
-        start_server(kill_session=True)
+        self.start_server(kill_session=True)
 
-    elif args.cmd:
-        with rcon:
-            print(rcon.cmd(args.cmd))
+    def exec_cmd(self, cmd: str) -> str:
+        """
+        Executes a command on the server via RCON and returns the response string.
+        """
+
+        with self._create_rcon_connection() as rcon:
+            return rcon.cmd(cmd)
 
 
 if __name__ == '__main__':
@@ -104,8 +115,26 @@ if __name__ == '__main__':
     actions.add_argument('--restart', '-r', action='store_true', help='restart server')
     actions.add_argument('--start', '-s', action='store_true', help='start server')
     actions.add_argument('--cmd', '-c', metavar='COMMAND', help='send RCON command')
-    parser.add_argument('--password', '-p', required=True, help='the server RCON password')
+    parser.add_argument('--pwfile', '-p', default=None, help='a file containing the server RCON password')
     parser.add_argument('--host', '-H', default='localhost', help='the RCON port. Default: localhost')
-    parser.add_argument('--port', '-P', default=25575, help='the RCON host. Default: 25575')
+    parser.add_argument('--port', '-P', type=int, default=25575, help='the RCON host. Default: 25575')
     args = parser.parse_args()
-    main(args)
+
+    password = None
+    if args.pwfile:
+        with open(args.pwfile) as file:
+            password = file.readline()
+
+    ctl = ServerCtl(args.host, args.port, password)
+
+    if args.start:
+        ctl.start_server()
+
+    elif args.notify_restart:
+        ctl.notify_restart(args.notify_restart)
+
+    elif args.restart:
+        ctl.restart()
+
+    elif args.cmd:
+        print(ctl.exec_cmd(args.cmd))
